ddf3147c56b7c40a75f8767a9636a3c9e70cbf44
jackb-imburse <84081937+jackb-imburse@users.noreply.github.com>
2022-07-27 00:54:02 +0100

    Feat: Order_CalculateScheduleStep3_V2 functions and send ScheduleInstructionStep3V2 protobuf message

--- asb.go
+++ asb.go
@@ -2,13 +2,51 @@ package asb
 
 import (
 	"context"
+	"log"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/messaging/azservicebus"
 	"go.k6.io/k6/js/modules"
+	"google.golang.org/protobuf/proto"
+
+	pb "github.com/jackb-imburse/xk6-azure-service-bus/contracts/order_management"
 )
 
 func init() {
 	modules.Register("k6/x/azure-service-bus", new(Asb))
+}
+
+func convertDateToCSDateTime(date time.Time) *pb.DateTime {
+	datetime := &pb.DateTime{
+		Value: date.Unix() / 60 / 60 / 24,
+		Scale: 0,
+		Kind:  0,
+	}
+	return datetime
+}
+
+func convertUintToDecimal(value uint64) *pb.Decimal {
+	decimal := &pb.Decimal{
+		Lo:        value,
+		Hi:        0,
+		SignScale: 0,
+	}
+	return decimal
+}
+
+func convertStringToGuid(guidStr string) *pb.Guid {
+	s := strings.Replace(guidStr, "-", "", -1)
+	lo := s[12:16] + s[8:12] + s[0:8]
+	hi := s[30:] + s[28:30] + s[26:28] + s[24:26] + s[22:24] + s[20:22] + s[18:20] + s[16:18]
+	a, _ := strconv.ParseUint(lo, 16, 64)
+	b, _ := strconv.ParseUint(hi, 16, 64)
+	guid := &pb.Guid{
+		Lo: a,
+		Hi: b,
+	}
+	return guid
 }
 
 type Asb struct {
@@ -37,6 +75,83 @@ func (a *Asb) SendMessage(queue string, correlationId string, subject string, bo
 		Subject:       &subject,
 		Body:          []byte(bodyDecoded),
 	}
+	err = sender.SendMessage(context.TODO(), sbMessage, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (a *Asb) Order_CalculateScheduleStep3_V2(
+	correlationId string,
+	tenantId string,
+	customerRef string,
+	financialInstrumentId string,
+	workflowId string,
+	orderRef string,
+	instructionRef string,
+	country string,
+	currency string,
+	amount uint64,
+	settledByDate string,
+	appId string,
+	appQueue string,
+	paymentMethod string,
+	client *azservicebus.Client) {
+
+	intYear, err := strconv.Atoi(settledByDate[0:4])
+	if err != nil {
+		panic(err)
+	}
+	intMonth, err := strconv.Atoi(settledByDate[5:7])
+	if err != nil {
+		panic(err)
+	}
+	intDay, err := strconv.Atoi(settledByDate[8:10])
+	if err != nil {
+		panic(err)
+	}
+
+	msg := &pb.ScheduleInstructionStep3V2{
+		CorrelationId:               correlationId,
+		RequestId:                   convertStringToGuid("30f6432c-ae25-4fbf-bd80-3b537e8ef0f2"),
+		TenantId:                    convertStringToGuid(tenantId),
+		CustomerRef:                 customerRef,
+		FinancialInstrumentId:       convertStringToGuid(financialInstrumentId),
+		WorkflowId:                  workflowId,
+		OrderRef:                    orderRef,
+		InstructionRef:              instructionRef,
+		Country:                     country,
+		Currency:                    currency,
+		Amount:                      convertUintToDecimal(amount),
+		RequestedSettlementDate:     convertDateToCSDateTime(time.Date(intYear, time.Month(intMonth), intDay, 0, 0, 0, 0, time.UTC)),
+		ForecastedSettlementDate:    convertDateToCSDateTime(time.Date(intYear, time.Month(intMonth), intDay, 0, 0, 0, 0, time.UTC)),
+		ScheduledExecutionTimestamp: convertDateToCSDateTime(time.Date(intYear, time.Month(intMonth), intDay, 1, 0, 0, 0, time.UTC)),
+		AppId:                       appId,
+		AppQueue:                    appQueue,
+		PaymentMethod:               paymentMethod,
+	}
+
+	out, err := proto.Marshal(msg)
+	if err != nil {
+		log.Fatalln("Failed to encode msg:", err)
+	}
+
+	targetQueue := "transaction"
+
+	sender, err := client.NewSender(targetQueue, nil)
+	if err != nil {
+		panic(err)
+	}
+	defer sender.Close(context.TODO())
+
+	subject := "ORDER:CALCULATE_SCHEDULE_STEP_3:V2"
+
+	sbMessage := &azservicebus.Message{
+		CorrelationID: &correlationId,
+		Subject:       &subject,
+		Body:          out,
+	}
+
 	err = sender.SendMessage(context.TODO(), sbMessage, nil)
 	if err != nil {
 		panic(err)
--- go.mod
+++ go.mod
@@ -1,3 +1,31 @@
 module github.com/jackb-imburse/xk6-azure-service-bus
 
 go 1.18
+
+require (
+	github.com/Azure/azure-sdk-for-go/sdk/messaging/azservicebus v1.0.2
+	go.k6.io/k6 v0.39.0
+	google.golang.org/protobuf v1.28.0
+)
+
+require (
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.0.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/internal v1.0.0 // indirect
+	github.com/dlclark/regexp2 v1.4.1-0.20201116162257-a2a8dda75c91 // indirect
+	github.com/dop251/goja v0.0.0-20220516123900-4418d4575a41 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/go-sourcemap/sourcemap v2.1.4-0.20211119122758-180fcef48034+incompatible // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/nxadm/tail v1.4.8 // indirect
+	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
+	github.com/serenize/snaker v0.0.0-20201027110005-a7ad2135616e // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/spf13/afero v1.1.2 // indirect
+	golang.org/x/net v0.0.0-20220531201128-c960675eff93 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20220411224347-583f2d630306 // indirect
+	gopkg.in/guregu/null.v3 v3.3.0 // indirect
+)
